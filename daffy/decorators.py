19d2b88532932545d8e879db05999dd23fc04e0b
Janne Sinivirta <janne.sinivirta@fourkind.com>
2021-01-31 18:00:50 +0200

    fix (kw)args handling

--- daffy/decorators.py
+++ daffy/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+
 import pandas as pd
 
 
@@ -18,19 +19,21 @@ def _check_columns(df, columns):
 def df_out(columns=None):
     def wrapper_df_out(func):
         @wraps(func)
-        def wrapper(args, **kwargs):
-            result = func(args, kwargs)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
             assert isinstance(
                 result, pd.DataFrame
             ), f"Wrong return type. Expected pandas dataframe, got {type(result)}"
             if columns:
                 _check_columns(result, columns)
             return result
+
         return wrapper
+
     return wrapper_df_out
 
 
-def _get_parameter(name=None, *args, kwargs):
+def _get_parameter(name=None, *args, **kwargs):
     if not name:
         return args[0]
     return kwargs[name]
@@ -39,13 +42,15 @@ def _get_parameter(name=None, *args, kwargs):
 def df_in(name=None, columns=None):
     def wrapper_df_out(func):
         @wraps(func)
-        def wrapper(*args, kwargs):
-            df = _get_parameter(name, *args, kwargs)
+        def wrapper(*args, **kwargs):
+            df = _get_parameter(name, *args, **kwargs)
             assert isinstance(
                 df, pd.DataFrame
             ), f"Wrong parameter type. Expected pandas dataframe, got {type(df)}"
             if columns:
                 _check_columns(df, columns)
             return func(*args, **kwargs)
+
         return wrapper
+
     return wrapper_df_out
