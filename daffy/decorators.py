7455c71d90139030bd7b35e9169ff491d6e39595
Janne Sinivirta <janne.sinivirta@fourkind.com>
2021-02-22 09:04:24 +0200

    added docstrings

--- daffy/decorators.py
+++ daffy/decorators.py
@@ -20,6 +20,16 @@ def _check_columns(df: pd.DataFrame, columns: ColumnsDef) -> None:
 
 
 def df_out(columns: ColumnsDef = None) -> Callable:
+    """Decorator used to document a function that returns a Pandas DataFrame.
+    The return value will be validated in runtime.
+
+    Args:
+        columns (ColumnsDef, optional): List or dict that describes expected columns of the DataFrame. Defaults to None.
+
+    Returns:
+        Callable: Decorated function
+    """
+
     def wrapper_df_out(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: str, **kwargs: Any) -> Any:
@@ -43,6 +53,17 @@ def _get_parameter(name: Optional[str] = None, *args: str, **kwargs: Any) -> pd.
 
 
 def df_in(name: Optional[str] = None, columns: ColumnsDef = None) -> Callable:
+    """Decorator used to document a function parameter that is a Pandas DataFrame.
+    The parameter will be validated in runtime.
+
+    Args:
+        name (Optional[str], optional): Name of the parameter that contains a DataFrame. Defaults to None.
+        columns (ColumnsDef, optional): List or dict that describes expected columns of the DataFrame. Defaults to None.
+
+    Returns:
+        Callable: Decorated function
+    """
+
     def wrapper_df_out(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: str, **kwargs: Any) -> Any:
@@ -84,6 +105,17 @@ def _log_output(level: int, func_name: str, df: Any, include_dtypes: bool) -> No
 
 
 def df_log(level: int = logging.DEBUG, include_dtypes: bool = False) -> Callable:
+    """Decorator used to wrap a function that consumes or produces a Pandas DataFrame or both.
+    Logs the columns of the consumed and/or produced DataFrame.
+
+    Args:
+        level (int, optional): Level of the logging messages produced. Defaults to logging.DEBUG.
+        include_dtypes (bool, optional): When set to True, will log also the dtypes of each column. Defaults to False.
+
+    Returns:
+        Callable: Decorated function.
+    """
+
     def wrapper_df_log(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: str, **kwargs: Any) -> Any:
