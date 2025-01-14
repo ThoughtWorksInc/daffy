126a9ffd524b3efce0301803b3ed3fee7fe2b129
Janne Sinivirta <ext.janne.sinivirta@rovio.com>
2025-01-14 14:12:03 +0200

    use explicit export and remove unnecessary version tag

--- daffy/__init__.py
+++ daffy/__init__.py
@@ -6,8 +6,8 @@ annotate your functions so that they document themselves and that documentation 
 the input and output on runtime.
 """
 
-__version__ = "0.5.0"
+from .decorators import df_in
+from .decorators import df_log
+from .decorators import df_out
 
-from .decorators import df_in  # noqa
-from .decorators import df_log  # noqa
-from .decorators import df_out  # noqa
+__all__ = ["df_in", "df_out", "df_log"]
